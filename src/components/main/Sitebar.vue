c6f45e04e41e1d6c05dd424965fe77039bfcf7d8
LeonID-11 <rleonidr@gmail.com>
2020-04-15 00:12:28 +0300

    1 commit

---
+++ src/components/main/Sitebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{'open': value}">
+
+      <router-link
+        v-for="link of links"
+        :key="link.url"
+        tag="li"
+        active-class="active"
+        :to="link.url"
+        :exact="link.exact"
+      >
+          <a :href="link.url" class="waves-effect waves-orange pointer">{{link.name}}</a>
+      </router-link>
+
+  </ul>
+</template>
+<script>
+export default {
+    data: ()=>({
+      links:[
+        {name:"Счет", url: "/", exact: true},
+        {name:"История", url: "/history"},
+        {name:"Планирование", url: "/planning"},
+        {name:"Новая запись", url: "/record"},
+        {name:"Категории", url: "/category"}
+      ]
+    }),
+    props:['value']
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app-main-layout">
+        <Navbar @navbar="openNavbar = !openNavbar"/>
+        <Sitebar v-model="openNavbar"/>
+
+        <main class="app-content" :class="{'full': !openNavbar}">
+            <div class="app-page">
+                <router-view />
+            </div>
+        </main>
+
+        <div class="fixed-action-btn">
+            <a class="btn-floating btn-large blue" href="#">
+                <i class="large material-icons">add</i>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/main/Navbar'
+import Sitebar from '@/components/main/Sitebar'
+export default {
+    data: ()=>({
+        openNavbar: true,
+    }),
+    components:{
+        Navbar, Sitebar
+    },
+    
+}
+</script>
